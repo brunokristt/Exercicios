5dc34455ae4526a6488d5580ae25c1a5c8ed9000
brunokristt <brunokristt@gmail.com>
2021-08-28 16:39:48 -0300

    Adicionar arquivos de projeto.

---
+++ Ex1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+
+namespace Ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Console.WriteLine("Gerenciamento do Valor dos produtos");
+            Console.Write("Digite o código do produto: ");
+            int codigo = int.Parse(Console.ReadLine());
+            Console.Write("Digite a quantidade de peças: ");
+            double quantidade = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Digite o valor unitário de cada peça: ");
+            double valorPeca = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double CalculaValor(double quantidade, double valor)
+            {
+                double resultado = quantidade * valor;
+                return resultado;
+
+            }
+
+            double valorTotal = CalculaValor(quantidade, valorPeca);
+            Console.Write("Código: " + codigo);
+            Console.Write(" Valor Unitário: " + valorPeca);
+            Console.Write(" Valor total: " + valorTotal, ("F2") );
+
+           
+
+        }
+    }
+}
